76c0b9c9a55b6b0fe77a986deee5b5f9640d6f9d
Victor Ngoubayo <vngoubayo@drtel.com>
2023-05-31 13:02:30 -0500

    Test3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +9,34 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello! This is Victor, do we have a victory?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+// I want to see if I am able to commit
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello! This is Victor, do we have a victory?
+          Let's see if this is still working!
         </p>
         <a
           className="App-link"
