d9a4ab6730fcb2b9ce583d1baa2b55722f97259b
Victor Ngoubayo <vngoubayo@drtel.com>
2023-05-31 15:15:35 -0500

    removed commented code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-/*
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello! This is Victor, do we have a victory?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
-// I want to see if I am able to commit
 import logo from './logo.svg';
 import './App.css';
 
